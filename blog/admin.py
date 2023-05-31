61e036db4f94cbe78cd79544f756269bbca38bca
EAmbrozas <ernestasambrozas@gmail.com>
2023-05-31 11:21:22 +0000

    Modify admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -3,10 +3,25 @@ from .models import Category, Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content']
+    list_display = ('title', 'content', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('created_on',)
+
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    search_fields = ['title', 'content']
+    list_display = ('title', 'slug', 'status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
 
 
-admin.site.register(Category)
-admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ['name', 'email', 'body']
