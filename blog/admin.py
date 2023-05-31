ca2df22f7742ae707ce8049e9b4120161067527c
EAmbrozas <ernestasambrozas@gmail.com>
2023-05-31 10:55:33 +0000

    Add summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Category, Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+
 
 admin.site.register(Category)
-admin.site.register(Post)
 admin.site.register(Comment)
