71cd67665adb6a8eaa10fd974cb44a43c8821989
EAmbrozas <ernestasambrozas@gmail.com>
2023-05-31 18:49:43 +0000

    Update allowed hosts

--- fitfinity/settings.py
+++ fitfinity/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["fitfinity.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['fitfinity.herokuapp.com', '8000-eambrozas-fitfinity-uswcgygaalo.ws-eu98.gitpod.io']
 
 
 # Application definition
