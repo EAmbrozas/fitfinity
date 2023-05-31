d0ec9d40e613ff5613ae869f27318c74411739df
EAmbrozas <ernestasambrozas@gmail.com>
2023-05-31 19:39:01 +0000

    Create home with category view and url for each view with templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+categories = [
+    {'id': 1, 'Title': 'All about training'},
+    {'id': 2, 'Title': 'All about exercises'},
+    {'id': 3, 'Title': 'All about diet'},
+]
+
+
+def home(request):
+    context = {'categories': categories}
+    return render(request, 'home.html', context)
+
+
+def category(request):
+    context = {'categories': categories}
+    return render(request, 'category.html', context)
